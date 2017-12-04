6b439733d96b1011cf6e22e8f4289a356498d89f
Patrick Smith <patrick@burntcaramel.com>
2017-12-04 14:56:04 +1100

    Add betterer error handling

--- api/server.js
+++ api/server.js
@@ -10,5 +10,10 @@ server.use(bodyParser.json()) // Allows me to have JSON uploads (POST/PUT/PATCH)
 
 // Start the server
 server.listen(7000, (error) => {
-  console.log('Server started at http://localhost:7000/')
+  if (error) {
+    console.error('Error starting', error)
+  }
+  else {
+    console.log('Server started at http://localhost:7000/')
+  }
 })
