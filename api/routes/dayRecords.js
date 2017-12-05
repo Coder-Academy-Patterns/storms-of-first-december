c60eb1cb4da36c6cfbebb9ac9f1b4701119ab3da
Patrick Smith <patrick@burntcaramel.com>
2017-12-05 12:01:26 +1100

    Add routes for day records in a particular city and month

--- api/routes/dayRecords.js
+++ api/routes/dayRecords.js
@@ -13,4 +13,32 @@ router.get('/day-records', (req, res) => {
     })
 })
 
+router.get('/day-records/:cityName', (req, res) => {
+  const { cityName } = req.params
+  DayRecord.find({ city: cityName })
+    .then((records) => {
+      res.json(records)
+    })
+    .catch((error) => {
+      res.json({ error: error.message })
+    })
+})
+
+router.get('/day-records/:cityName/:year/:month', (req, res) => {
+  const { cityName, year, month } = req.params
+  DayRecord.find({
+    city: cityName,
+    day: {
+      $gte: new Date(year, month, 1),
+      $lt: new Date(year, month + 1, 1)
+    }
+  })
+    .then((records) => {
+      res.json(records)
+    })
+    .catch((error) => {
+      res.json({ error: error.message })
+    })
+})
+
 module.exports = router
